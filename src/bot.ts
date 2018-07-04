ca058fcf8715dd1bd576d4a907fc8fd36ed363bf
Paul Blouët <paul@paulblouet.fr>
2018-07-04 19:21:27 +0200

    initial and v1

---
+++ src/bot.ts
@@ -0,0 +1,23 @@
+import { Client, Message } from "discord.js";
+import priv from "./private.json";
+
+const client = new Client();
+
+client.on("ready", () => {
+    process.stdout.write("ready!");
+});
+
+client.on("message", async (message) => {
+    if (message.content === "/simple") {
+        const response = await message.channel.send("Quel jour ?") as Message;
+        await response.react("🇱");
+        await response.react("🇦");
+        await response.react("🇪");
+        await response.react("🇯");
+        await response.react("🇻");
+        await response.react("🇸");
+        await response.react("🇩");
+    }
+});
+
+client.login(priv.TOKEN);
